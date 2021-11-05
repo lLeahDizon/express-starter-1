b498ebfce8c569ece0bcd8a9458fc545f65afdbc
lLeahDizon <516330797@qq.com>
2021-11-05 20:28:32 +0800

    引入 ts

---
+++ app2.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/xxx', (request, response) => {
+  response.send('你好');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
