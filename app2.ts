5a0b0c1c4d9b3d66f6cd10de99d229376cd101dd
leahdizon <gejing199278er>
2021-12-17 22:08:06 +0800

    refactor: 更新依赖

--- app2.ts
+++ app2.ts
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
-app.get('/xxx', (request, response) => {
+app.get('/', (request, response) => {
   response.send('你好');
 });
+
+app.post('/xxx', (request, response, next) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
