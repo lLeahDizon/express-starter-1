0527d2427dc9aa72eb3200cafa6a816b4a41de50
Lemon <516330797@qq.com>
2022-05-26 00:15:54 +0800

    feat: next 传参错误处理

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ const app = express()
 
 app.use(logger('dev'))
 app.use((request, response, next) => {
-  if (request.path === '/') {
-    response.send('根目录')
-  }
+  response.write('1')
+  next()
+})
+app.use((request, response, next) => {
+  response.write('2')
+  if (true) {
+    next('未登录')
+  } else
+    next()
+})
+
+app.use((request, response, next) => {
+  response.write('3')
   next()
 })
 
-// app.get('/xxx', (request, response, next) => {
-//   response.send('xxx 页面')
-//   next()
-// })
-
-app.route('/xxx')
-  .all(() => {})
-  .get((request, response, next) => {
-    response.send('xxx')
-  })
-  .post()
+app.use((error, request, response, next) => {
+  response.write(error)
+  response.end()
+  next()
+})
 
 app.listen(3000, () => {
   console.log('正在 listen 3000')
